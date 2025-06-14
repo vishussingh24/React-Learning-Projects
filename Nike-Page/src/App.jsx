7b81aa8da0cfb63b1f584feb3e6880576c49b365
Your Name <vishal241330@gmail.com>
2025-06-14 17:48:32 +0530

    First-Commit

---
+++ Nike-Page/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="../public/images/logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+
+        <button>Login</button>
+
+      </nav>
+    </div>
+  )
+}
+
+export default App
