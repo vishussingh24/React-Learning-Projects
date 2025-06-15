d92dfef65ce5709ac8ac744f15f944ab1e45d81b
Your Name <vishal241330@gmail.com>
2025-06-15 16:07:57 +0530

    Completed-Nike-Page

--- Nike-Page/src/App.jsx
+++ Nike-Page/src/App.jsx
@@ -1,24 +1,14 @@
 import './App.css'
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <div>
-      <nav>
-        <div className="logo">
-          <img src="../public/images/logo.png" alt="logo" />
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-
-        <button>Login</button>
-
-      </nav>
-    </div>
+    <>
+      <Navigation />
+      <Hero />
+    </>
   )
-}
+}; 
 
 export default App
